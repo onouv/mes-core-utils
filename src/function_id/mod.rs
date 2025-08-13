af78299785d069402537817c1df91a28adbbe4bc
olaf <olaf.nouvortne@onosoft.online>
2025-08-13 11:13:23 +0200

    .

---
+++ src/function_id/mod.rs
@@ -0,0 +1,46 @@
+mod tests;
+
+use std::fmt;
+use std::fmt::{Debug, Display, Formatter};
+
+use super::plant_item_id_error::*;
+use crate::plant_item_id::{ID_GROUP_LEN_DEFAULT, ID_SEGMENT_DELIMITER_DEFAULT, PlantItemId};
+
+pub const FUNCTION_ID_PREFIX: char = '-';
+
+#[derive(PartialEq, PartialOrd)]
+pub struct FunctionId {
+    item_id: PlantItemId,
+}
+
+impl FunctionId {
+    pub fn new(seg_delimiter: char, group_len: usize, id: &str) -> Result<Self, PlantItemIdError> {
+        if 
+        let item_id = PlantItemId::new(seg_delimiter, id)?;
+
+        Ok(Self { item_id })
+    }
+}
+
+impl Display for FunctionId {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        self.item_id.fmt(f)
+    }
+}
+
+impl Default for FunctionId {
+    fn default() -> Self {
+        // we carefully provide parameters secured at compile time
+        // to this call to ensure that it will not fail, unless
+        // something is fundamentally broken in which case we deliberatly panic
+        let item_id = PlantItemId::new(ID_SEGMENT_DELIMITER_DEFAULT, "-001").unwrap();
+
+        Self { item_id }
+    }
+}
+
+impl Debug for FunctionId {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        self.item_id.fmt(f)
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -3,3 +3,6 @@ use crate::plant_item_id::*;
 
 pub mod equipment_id;
 pub use crate::equipment_id::*;
+
+pub mod function_id;
+pub use crate::function_id::*;
